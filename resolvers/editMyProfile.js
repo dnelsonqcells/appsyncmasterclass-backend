4b2b6f431c1375e10bb7c8f8ed9290220647f1dc
dnelsonqcells <david.nelson@qcells.com>
2024-03-28 14:38:37 -0400

    feat: Add get-upload-url.js

--- resolvers/editMyProfile.js
+++ resolvers/editMyProfile.js
@@ -2,16 +2,15 @@ import { update } from '@aws-appsync/utils/dynamodb';
 import { util } from '@aws-appsync/utils';
 
 export function request(ctx) {
+    const updateFields = {}
+
+    Object.keys(ctx.arguments.newProfile).forEach(key => {
+        updateFields[key] = ctx.arguments.newProfile[key]
+    })
     return update({
         key: {id: ctx.identity.username},
         update: {
-            name: ctx.arguments.newProfile.name,
-            imageUrl: ctx.arguments.newProfile.imageUrl,
-            backgroundImageUrl: ctx.arguments.newProfile.backgroundImageUrl,
-            bio: ctx.arguments.newProfile.bio,
-            location: ctx.arguments.newProfile.location,
-            website: ctx.arguments.newProfile.website,
-            birthdate: ctx.arguments.newProfile.birthdate,
+            ...updateFields
         }
     })
 }
